224f17cdcc314a0573c311a8d97cc5b27d6bcc87
khamiltonuk <krisham87@hotmail.com>
2014-07-29 12:21:02 +0100

    tidy up code, got auto play mode to work, update readme

--- README.md
+++ README.md
@@ -1,42 +1,16 @@
-//http://www.playrps.com/
-#Frontend Developer Test
+#Rock paper Scissors lizard spock
+live example http://jsfiddle.net/kristianhamilton/KLLQD/embedded/result/
 
-Before we conduct any interview we ask all potential front end developers to complete a simple technical task.
+##Whats going on
+-	The function compareChoices() compares avaialble choices within an array, theses options are in a specific order. The choices given are then compared based on their position in this order and these numbers are taken away from each other. This differance is then used to work out the game result.
+-	As all the action happens on one page I have used buttons rather than a tags.
+-	I have created a series of function that can be reused in various locations.
+-	High level accesibiltiy compliance is something I am keen to further explore, you will notice a few aria roles.
+-	I have used jQuery, I will often throw in a splash of vanillia javascript for efficiency, I have used Jquery framework for browser support, http://youmightnotneedjquery.com/ is a cool resorce to show how to do certain tasks without using jQuery and http://www.quirksmode.org/dom/events/index.html to check wider support
+-	I have used a custom build of jQuery so I have just what I want to reduce weight.
+-	Hand images taken from //http://www.playrps.com/ to save time.
 
-Please include a brief description of your solution and what decisions you made in its creation.
-
-##User Story
-
-Create a browser based version of the game ‘Rock, Paper, Scissors’.
-
-Don't know the game? http://en.wikipedia.org/wiki/Rock-paper-scissors
-
-##Acceptance Criteria
-
--	Ability to play against the computer
--	Ability to simulate a game (Computer vs Computer)
--	Ability to restart the game
--	Computer generated plays need to be random
-
-##Guidelines
-
--	The UI can be as simple or as complex as you wish
-
--	We are keen to see how much you think is enough, and how much would go into a Minimum Viable Product.  As a guide, elegant and simple wins over feature rich every time, though extra gold stars are given to people who get excited and do more because they are having fun.
-
--	We also consider the extensibility of the code produced.  Well factored code should be relatively easily extended. http://en.wikipedia.org/wiki/Rock-paper-scissors-lizard-Spock may be a natural extension.
-
--	Bonus points for good accessibility, JavaScript unit tests, Responsive and well commented code
-
-
-
-
-##Technical Requirements
-
--	jQuery or equivalent library’s are allowed (no plugins) but it is important for us to see that you can write pure JavaScript. We want to see your code, not someone elses.
--	Sass / Less are allowed
--	The solution should work in IE9+ and all modern browsers, bonus points ie8 and lower
-
-##Summary:
-
-Alongside this document you should find a few empty files that you can build your solution in. Feel free to change the structure or add new files as you see fit.
+##Further development
+-	Stagger automated computer vs computer game play, to watch it happen rather than all at once.
+-	Use module pattern... but is already wrapped in a function to prevent poluting the global namespace.
+-	Allow user to swap between rock paper scissors mode and rock paper Scissors lizard spock.
--- js/main.js
+++ js/main.js
@@ -17,46 +17,61 @@ var rockPaperScissors = function(){
         wins = lose = draws = 0;
         $('#left-hand, #right-hand').removeClass();
         renderScore();
-        $('#message').addClass('hidden');
+        $('#message').addClass('hidden').attr('aria-hidden', 'true');
         $('#js-set-up').removeClass('disabled');
         $('#js-reset').text('Reset');
-        $('#js-auto-play').removeClass('hidden');
+        $('#js-auto-play').removeClass('hidden').attr('aria-hidden', 'false');
     };
     var checkStatus = function(){
-        if (wins === (gameMode - 1 ) || lose === 2){
+        if (wins === (gameMode - 1 ) || lose === (gameMode - 1 )){
             //Game over
             if (wins === (gameMode - 1 ) ){
-                displayMessage('you win');
+                displayMessage('You win');
             }else if (lose === 2) {
-                displayMessage('you lose');
+                displayMessage('You lose');
             }
-            $('#js-auto-play').addClass('hidden');
+            $('#js-auto-play').addClass('hidden').attr('aria-hidden', 'true');
             $('#js-set-up').addClass('disabled');
             $('#js-reset').text('Play Again');
         }
     };
+    var gameOver = function(){
+
+    };
     var displayMessage = function(message, messageDuration){
-        if ($('#message')){
-            $('#message').text(message).removeClass('hidden');
+        if ($('#message').length){
+            $('#message').text(message).removeClass('hidden').attr('aria-hidden', 'false');
         } else {
-            $jsBattleDome.append('<p id="message" class="message-text">' + message + '</p>');
+            $jsBattleDome.append('<p id="message" aria-hidden="false" role="alert" class="message-text">' + message + '</p>');
         }
         if (messageDuration){
             setTimeout(function() {
-                $('#message').addClass('hidden');
+                $('#message').addClass('hidden').attr('aria-hidden', 'true');
             }, messageDuration );
         }
     };
     var renderGamePlay = function(userChoice, randomChoice) {
         $('#left-hand, #right-hand').removeClass().addClass('bounce animated');
         setTimeout(function() {
-            $('#left-hand').removeClass().addClass(userChoice);
-            $('#right-hand').removeClass().addClass(randomChoice);
+            $('#left-hand').removeClass().addClass(userChoice).text('player one plays: ' + userChoice);
+            $('#right-hand').removeClass().addClass(randomChoice).text('player two plays: ' + randomChoice);
             renderScore();
-            checkStatus();
+            if (gameMode != 0){
+                checkStatus();
+            }
         }, 1000);
     };
-    var bigBang = function(choice1, choice2) {
+    var activateSettingPanel =function(){
+        $('#settings-pane').toggleClass('active');
+        if ($('#settings-pane').hasClass('active')){
+            $('#js-settings').text('close');
+            $('#message, #js-reset, .actions').addClass('hidden').attr('aria-hidden', 'true');
+        } else {
+            $('#js-settings, #js-reset, .actions').attr('aria-hidden', 'true').removeClass('hidden');
+            $('#js-settings').text('settings');
+        }
+    }
+    var compareChoices = function(choice1, choice2) {
         var index1 = options.indexOf(choice1),
             index2 = options.indexOf(choice2),
             dif = index2 - index1;
@@ -82,41 +97,34 @@ var rockPaperScissors = function(){
     };
     $('input[name=game-mode]').on('change',function(){
         gameMode = parseInt($('input[name=game-mode]:checked').val());
+        resetGame();
     });
 
     $jsBattleDome.on('click', function(e){
         var target = e.target.id;
-        if (target == 'js-settings'){
-            $('#settings-pane').toggleClass('active');
-            if ($('#settings-pane').hasClass('active')){
-                $('#js-settings').text('close');
-                $('#message').addClass('hidden');
-                $('#js-reset, .actions').stop(true,true).fadeOut();
-            } else {
-                $('#js-settings').text('settings');
-                $('#js-reset, .actions').stop(true,true).fadeIn();
-            }
+        if (target === 'js-settings'){
+            activateSettingPanel();
         }
-        if (target == 'js-reset'){
+        if (target === 'js-reset'){
             resetGame();
         }
-        if (target == 'js-auto-play'){
+        if (target === 'js-auto-play'){
             var autoPlay = function(){
                 var standin = options[Math.floor(Math.random() * 5)],
                     randomChoice = options[Math.floor(Math.random() * 5)];
-                console.log(bigBang(standin, randomChoice));
+                    console.log(compareChoices(standin, randomChoice));
+                if (wins < (gameMode - 1 ) && lose < (gameMode - 1 )){
+                    return autoPlay();
+                }
             };
-            while (wins < (gameMode - 1 ) || lose < (gameMode - 1 )){
-                autoPlay();
-            }
+            autoPlay();
         }
         if ($(e.target).parent().hasClass('actions')){
             var userChoice = options[$(e.target).index()],
                 randomChoice = options[Math.floor(Math.random() * 5)];
-            console.log(bigBang(userChoice, randomChoice));
+            console.log(compareChoices(userChoice, randomChoice));
         }
     });
-
     
     /*if (gameType === 'rps'){
         options = ['paper', 'rock', 'scissors']
